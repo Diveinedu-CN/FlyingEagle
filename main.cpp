6d87b4f115bc8e333d4666465c4453f46cdb2380
Cheetah <memccpy@gmail.com>
2013-09-01 19:09:53 +0800

    fix Microsoft YaHei font problem in EGLFS platform

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <QtGui/QGuiApplication>
+#include <QFontDatabase>
 #include "qtquick2applicationviewer.h"
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
+#ifdef Q_OS_LINUX
+    QFontDatabase fontDB;
+    qDebug()<<"ScreenSize:"<<"Fonts:"<<fontDB.families();
+    QFont font = app.font();
+    //设置程序全局字体为微软雅黑,EGLFS平台插件的情况下,字体文件必须在$QTDIR/lib/fonts/下
+    font.setFamily(QString("Microsoft YaHei"));
+    app.setFont(font);
+#endif
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/FlyingEagle/main.qml"));
     viewer.showExpanded();
